3fe2c4b25947c55d2080b64bdadaf75fef3d5a33
Varin Nair <varinnair@gmail.com>
2023-04-04 14:23:36 -0400

    Add types for essays and chunks

---
+++ types/index.ts
@@ -0,0 +1,15 @@
+export type NavalEssay = {
+    title: string;
+    url: string;
+    content: string;
+    tokens: number;
+    chunks: NavalChunks[];
+};
+
+export type NavalChunks = {
+    essay_title: string;
+    essay_url: string;
+    content: string;
+    tokens: number;
+    embedding: number[];
+};
