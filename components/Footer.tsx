b06a44df56bdf013f3171acf015c02d13ee9c688
Varin Nair <varinnair@gmail.com>
2023-04-07 02:00:12 -0400

    Remove text from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,30 +3,8 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
     return (
-        <div className="flex h-[50px] border-t border-gray-300 py-2 px-8 items-center sm:justify-between justify-center">
+        <div className="flex py-2 px-8 border-t border-gray-300 items-center sm:justify-between justify-center">
             <div className="hidden sm:flex"></div>
-
-            <div className="hidden sm:flex italic text-sm">
-                Created by
-                <a
-                    className="hover:opacity-50 mx-1"
-                    href="https://twitter.com/varin_nair"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Varin Nair
-                </a>
-                based on the
-                <a
-                    className="hover:opacity-50 ml-1"
-                    href="https://www.navalmanack.com/almanack-of-naval-ravikant/table-of-contents"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Almanack of Naval Ravikant
-                </a>
-                .
-            </div>
 
             <div className="flex space-x-4">
                 <a
@@ -35,7 +13,7 @@ export const Footer: FC = () => {
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <IconBrandTwitter size={24} />
+                    <IconBrandTwitter size={32} />
                 </a>
 
                 <a
@@ -44,7 +22,7 @@ export const Footer: FC = () => {
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <IconBrandGithub size={24} />
+                    <IconBrandGithub size={32} />
                 </a>
             </div>
         </div>
